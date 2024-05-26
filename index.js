35491ac13c726cbbd364dd25159b89c60b149e15
Sameth Sim <55465884+samethsim@users.noreply.github.com>
2024-05-26 00:50:24 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let scoreHome = getEl("score-home");
+let scoreGuest = getEl("score-guest");
+
+//score
+let homeScore=0;
+let guestScore=0;
+
+getEl("home-1-btn").addEventListener("click", ()=>{
+    addScore(scoreHome,1)
+})
+
+getEl("home-2-btn").addEventListener("click", ()=>{
+    addScore(scoreHome,2)
+})
+getEl("home-3-btn").addEventListener("click", ()=>{
+    addScore(scoreHome,3)
+})
+
+getEl("guest-1-btn").addEventListener("click", ()=>{
+    addScore(scoreGuest,1)
+})
+getEl("guest-2-btn").addEventListener("click", ()=>{
+    addScore(scoreGuest,1)
+})
+getEl("guest-3-btn").addEventListener("click", ()=>{
+    addScore(scoreGuest,1)
+})
+function getEl(id) {
+    return document.getElementById(`${id}`);
+}
+
+function addScore(elementId,points) {
+    if (elementId.getAttribute("id")==="score-home"){
+        homeScore += points;
+        elementId.textContent = homeScore;
+    }
+    else {
+        guestScore += points;
+        elementId.textContent = guestScore;
+    }
+    
+}
